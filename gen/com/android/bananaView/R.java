ffa1aaedb9c922e1972060951ba75bc4faa64bac
Josue <josue.chaqui@gmail.com>
2014-05-27 18:40:57 -0600

    error corregido y creados los labels

--- gen/com/android/bananaView/R.java
+++ gen/com/android/bananaView/R.java
@@ -8,71 +8,143 @@
 package com.android.bananaView;
 
 public final class R {
-	public static final class attr {
-	}
+    public static final class attr {
+        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+         */
+        public static final int buttonBarButtonStyle=0x7f010001;
+        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+         */
+        public static final int buttonBarStyle=0x7f010000;
+    }
+    public static final class color {
+        public static final int black_overlay=0x7f040000;
+    }
+    public static final class dimen {
+        public static final int activity_marge_horizontal=0x7f050001;
+        public static final int activity_marge_vertical=0x7f050002;
+        public static final int text_size_standard=0x7f050000;
+    }
+    public static final class drawable {
+        public static final int ic_launcher=0x7f020000;
+    }
+    public static final class id {
+        public static final int lbCartoon=0x7f090006;
+        public static final int lbCineCanal=0x7f090009;
+        public static final int lbFoxSports=0x7f090003;
+        public static final int lbGuatevision=0x7f090002;
+        public static final int lbHistory=0x7f090004;
+        public static final int lbMtv=0x7f090008;
+        public static final int lbNatGeo=0x7f090005;
+        public static final int lbTheFilmZOne=0x7f09000a;
+        public static final int lbTreceVision=0x7f090007;
+        public static final int lbWarnerChannel=0x7f09000b;
+        public static final int lbl=0x7f090000;
+        public static final int menu_settings=0x7f09000c;
+        public static final int myScrollView=0x7f090001;
+    }
+    public static final class layout {
+        public static final int activity_json=0x7f030000;
+    }
+    public static final class menu {
+        public static final int activity_json=0x7f080000;
+    }
+    public static final class string {
+        public static final int CartoonNetwork=0x7f06000e;
+        public static final int History=0x7f06000c;
+        public static final int Mtv=0x7f060010;
+        public static final int NatGeo=0x7f06000d;
+        public static final int aceptar=0x7f060003;
+        public static final int app_name=0x7f060000;
+        public static final int cineCanal=0x7f060011;
+        public static final int dummy_button=0x7f060008;
+        public static final int dummy_content=0x7f060007;
+        public static final int foxSports=0x7f06000b;
+        public static final int guatevision=0x7f06000a;
+        public static final int hello_world=0x7f060001;
+        public static final int json=0x7f060005;
+        public static final int leer=0x7f060004;
+        public static final int menu_settings=0x7f060002;
+        public static final int theFilmZone=0x7f060012;
+        public static final int title_activity_primera=0x7f060009;
+        public static final int titulo=0x7f060006;
+        public static final int treceVision=0x7f06000f;
+        public static final int warnerChannel=0x7f060013;
+    }
+    public static final class style {
+        /** 
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
 
-	public static final class dimen {
-		public static final int activity_marge_horizontal = 0x7f040001;
-		public static final int activity_marge_vertical = 0x7f040002;
-		public static final int text_size_standard = 0x7f040000;
-	}
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
 
-	public static final class drawable {
-		public static final int ic_launcher = 0x7f020000;
-	}
+        Base application theme for API 11+. This theme completely replaces
+        AppBaseTheme from res/values/styles.xml on API 11+ devices.
+    
+ API 11 theme customizations can go here. 
 
-	public static final class id {
-		public static final int lbl = 0x7f080000;
-		public static final int menu_settings = 0x7f080001;
-	}
+        Base application theme for API 14+. This theme completely replaces
+        AppBaseTheme from BOTH res/values/styles.xml and
+        res/values-v11/styles.xml on API 14+ devices.
+    
+ API 14 theme customizations can go here. 
+         */
+        public static final int AppBaseTheme=0x7f070000;
+        /**  Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
+         */
+        public static final int AppTheme=0x7f070001;
+        public static final int ButtonBar=0x7f070004;
+        public static final int ButtonBarButton=0x7f070003;
+        public static final int FullscreenActionBarStyle=0x7f070006;
+        public static final int FullscreenTheme=0x7f070005;
+        public static final int layout=0x7f070002;
+    }
+    public static final class styleable {
+        /** 
+         Declare custom theme attributes that allow changing which styles are
+         used for button bars depending on the API level.
+         ?android:attr/buttonBarStyle is new as of API 11 so this is
+         necessary to support previous API levels.
+    
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #ButtonBarContainerTheme_buttonBarButtonStyle com.android.bananaView:buttonBarButtonStyle}</code></td><td></td></tr>
+           <tr><td><code>{@link #ButtonBarContainerTheme_buttonBarStyle com.android.bananaView:buttonBarStyle}</code></td><td></td></tr>
+           </table>
+           @see #ButtonBarContainerTheme_buttonBarButtonStyle
+           @see #ButtonBarContainerTheme_buttonBarStyle
+         */
+        public static final int[] ButtonBarContainerTheme = {
+            0x7f010000, 0x7f010001
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.android.bananaView.R.attr#buttonBarButtonStyle}
+          attribute's value can be found in the {@link #ButtonBarContainerTheme} array.
 
-	public static final class layout {
-		public static final int activity_json = 0x7f030000;
-	}
 
-	public static final class menu {
-		public static final int activity_json = 0x7f070000;
-	}
+          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+          @attr name android:buttonBarButtonStyle
+        */
+        public static final int ButtonBarContainerTheme_buttonBarButtonStyle = 1;
+        /**
+          <p>This symbol is the offset where the {@link com.android.bananaView.R.attr#buttonBarStyle}
+          attribute's value can be found in the {@link #ButtonBarContainerTheme} array.
 
-	public static final class string {
-		public static final int aceptar = 0x7f050003;
-		public static final int app_name = 0x7f050000;
-		public static final int hello_world = 0x7f050001;
-		public static final int json = 0x7f050005;
-		public static final int leer = 0x7f050004;
-		public static final int menu_settings = 0x7f050002;
-		public static final int titulo = 0x7f050006;
-	}
 
-	public static final class style {
-		/**
-		 * Base application theme, dependent on API level. This theme is
-		 * replaced by AppBaseTheme from res/values-vXX/styles.xml on newer
-		 * devices.
-		 * 
-		 * 
-		 * Theme customizations available in newer API levels can go in
-		 * res/values-vXX/styles.xml, while customizations related to
-		 * backward-compatibility can go here.
-		 * 
-		 * 
-		 * Base application theme for API 11+. This theme completely replaces
-		 * AppBaseTheme from res/values/styles.xml on API 11+ devices.
-		 * 
-		 * API 11 theme customizations can go here.
-		 * 
-		 * Base application theme for API 14+. This theme completely replaces
-		 * AppBaseTheme from BOTH res/values/styles.xml and
-		 * res/values-v11/styles.xml on API 14+ devices.
-		 * 
-		 * API 14 theme customizations can go here.
-		 */
-		public static final int AppBaseTheme = 0x7f060000;
-		/**
-		 * Application theme. All customizations that are NOT specific to a
-		 * particular API-level can go here.
-		 */
-		public static final int AppTheme = 0x7f060001;
-		public static final int layout = 0x7f060002;
-	}
+          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+          @attr name android:buttonBarStyle
+        */
+        public static final int ButtonBarContainerTheme_buttonBarStyle = 0;
+    };
 }
